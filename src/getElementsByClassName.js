72c58990c9c3c3d4a0d384d00ccc6923cb9ab2f7
ranched <jasosmc@yahoo.com>
2017-11-13 11:43:14 -0600

    Filled blank on spec, build structure for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,32 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function(className, node
 ) {
   // your code here
+  
+  //get first element, get classes, check if className is in classes
+  //find chilren
+  //iterate through children calling getElementsByClassName
+
+  var node = node || document.body;
+  var elements = [];
+  
+  if(node.classList.contains(className)){
+    elements.push(node);
+    console.log(node.childNodes);
+    
+  }
+  
+  var children = node.children;
+
+  if(children.length > 0){
+    for(i = 0; i < children.length; i++){
+      elements.concat(getElementsByClassName(className, children[i]));
+    }
+  }
+
+  return elements;
+   
+
 };
