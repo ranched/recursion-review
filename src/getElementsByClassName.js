6df8e560e60e2ce8c121fac715a75f65bf492b13
ranched <jasosmc@yahoo.com>
2017-11-13 16:06:25 -0600

    Finished getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,32 +4,52 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className, node
-) {
+var getElementsByClassName = function(className, nodeElement) {
   // your code here
+  console.log(new Date);
   
-  //get first element, get classes, check if className is in classes
+//get first element, get classes, check if className is in classes
   //find chilren
   //iterate through children calling getElementsByClassName
-
-  var node = node || document.body;
+/*
+  var body =  document.body;
   var elements = [];
-  
-  if(node.classList.contains(className)){
-    elements.push(node);
-    console.log(node.childNodes);
-    
-  }
-  
-  var children = node.children;
 
-  if(children.length > 0){
-    for(i = 0; i < children.length; i++){
-      elements.concat(getElementsByClassName(className, children[i]));
+  if(body.classList.contains(className)){
+    elements.push(body);
+  }
+
+  findChildren(body);
+  function findChildren(parent){
+  
+    var children = parent.children;
+    if(children.length > 0){
+      for(i = 0; i < children.length; i++){
+        if(children[i].classList !== undefined && children[i].classList.contains(className)){
+          elements.push(children[i]);
+        }
+        findChildren(children[i]);
+      }
+    }
+    }
+  
+  return elements;
+
+*/
+  var elements = [];
+  if(document.body.classList.contains(className)){
+    elements.push(document.body);
+  }   
+  getChildrenClasses(document.body);
+  function getChildrenClasses(parent){
+    var children = parent.childNodes;
+    for(var i = 0; i < children.length; i++){
+      if(children[i].classList !== undefined && children[i].classList.contains(className)){
+        elements.push(children[i]);
+      } 
+      getChildrenClasses(children[i]); 
     }
   }
-
   return elements;
-   
-
 };
+
